0de6ee36a1ce3b970e1ce3775973852a620b4763
Oli Le Maire <olilemaire85@gmail.com>
2021-04-28 17:18:46 +0100

    create debugging process document

---
+++ debugging_process.rb
@@ -0,0 +1,28 @@
+# Debugging Process
+
+
+1.	Have you understood the instructions properly
+
+2.	Have you understood the error message properly?
+
+3.	Write down what you understand from the error message in plain English
+
+4.	a) Try and solve the smallest part of the error
+b) Solve that part
+c) return to 'a'
+
+5.	Google the error if still struggling
+
+6.	If you are still struggling, do one or both of the following:
+a)	explain the whole code as if you are explaining the issue to a rubber duck, and see if you come across anything that doesn’t add up
+b)	draw a diagram of the code
+
+7.	List your assumptions about the code and see if these assumptions are correct
+
+8.	Print for visibility
+
+9.	Look out for None results. This will not necessarily be picked up by inserting things like “print(hello)” as you are looking for a ‘ghost’. For example something such as string[0] on this “hello” will not pick up “hello” but will only pick up “h”
+
+10.	Never assume. This will lead to a stressful spiral of guessing
+
+11.	Reach out (but only as a last resort), but before you end up guessing in a spiral of frustration
